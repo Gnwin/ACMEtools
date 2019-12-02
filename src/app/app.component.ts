d3b7c866e90c57847f34c3f4d1ab5f0af4862630
Godwin Nwachukwu <nwgwin@gmail.com>
2019-12-02 17:07:58 +0100

    added pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div><h1 class = "container">{{pageTitle}}</h1>
+  <div><h1 class="container">{{pageTitle}}</h1>
   <pm-products></pm-products>
 </div>
 `
